0c70847590a0e73240733f9d28ae415718dd9614
mcbobby123 <kellybob3737@gmail.com>
2023-12-01 22:39:51 -0700

    shorter

--- src/02/02.kt
+++ src/02/02.kt
@@ -1,14 +1,12 @@
 import kotlin.math.max
 
 fun main(){
-    val shared = readInput(true).lines().map { Regex("""(\d+) (red|green|blue)""").findAll(it)
-        .map { Pair(it.groupValues[1].toInt(), it.groupValues[2]) }.toList() }.mapIndexed{ ind, l ->
+    val s = readInput(true).lines().map { Regex("""(\d+) (red|green|blue)""").findAll(it)
+        .map { Pair(it.groupValues[1].toInt(), it.groupValues[2]) } }.map{
         val cts = DefaultMap<String,Int> {0}
-        for ((ct,clr) in l) cts.applyOn(clr) { max(it, ct) }
-        Pair(ind,cts)
+        for ((ct,clr) in it) cts.applyOn(clr) { max(it, ct) }
+        cts
     }
-    val p1 = shared.filter { (ind,it) -> it["red"] <= 12 && it["green"] <= 13 && it["blue"] <= 14 }.sumOf { (ind,_) -> ind+1 }
-    println(p1)
-    val p2 = shared.map { (ind,cts) -> cts.values.fold(1) { l,r -> l*r } }.sum()
-    println(p2)
+    println(s.indices.sumOf { if(s[it]["red"] <= 12 && s[it]["green"] <= 13 && s[it]["blue"] <= 14) it+1 else 0 })
+    println(s.sumOf { it.values.fold(1L) { l, r -> l * r } })
 }
