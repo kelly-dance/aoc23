96e180039814906ae28d090a4056628e422b63a1
mcbobby123 <kellybob3737@gmail.com>
2023-12-03 22:16:30 -0700

    shorter

--- src/04/04.kt
+++ src/04/04.kt
@@ -1,25 +1,13 @@
 import kotlin.math.min
 
 fun main(){
-    var inp = readInput(true).lines().map{
-        it.split(':')[1].split('|').map{
+    val inp = readInput(true).lines().map{
+        it.substring(it.indexOf(':')).split('|').map{
             Regex("""(\d+)""").findAll(it).toList().map { it.value.toLong() }
         }
-    }.map { (a,b) ->
-        val cts = DefaultMap<Long,Long>{0}
-        for(v in a) cts.applyOn(v){it+1}
-        var c=0
-        for(v in b) if(cts[v]>0){
-            c+=1
-            cts.applyOn(v){it-1}
-        }
-        c
-    }
+    }.map { (a,b) -> a.intersect(b).size }
     println(inp.sumOf { (1 shl it)/2 })
-    var cts = mutableListOf<Long>()
-    for(x in inp.indices) cts.add(1)
-    for(i in inp.indices) for(j in i+1 until min(i+1+inp[i], inp.size)){
-        cts[j] += cts[i]
-    }
+    val cts = inp.map { 1 }.toMutableList()
+    for(i in inp.indices) for(j in i+1 until min(i+1+inp[i], inp.size)) cts[j] += cts[i]
     println(cts.sum())
 }
